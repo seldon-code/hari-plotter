e26612f3f1d09b0cb44666785735c12b97cfe407
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-09-29 13:12:19 +0000

    Printing of logo to terminal

---
+++ ascii_logo.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+from rich_pixels import Pixels
+from PIL import Image
+from rich.console import Console
+
+
+def print_logo(text=False, width=60):
+    console = Console()
+
+    if not text:
+        file = Path(os.path.dirname(__file__)) / "res/logo.png"
+    else:
+        file = Path(os.path.dirname(__file__)) / "res/logo_text.png"
+
+    with Image.open(file) as image:
+        new_size = [int(width), int(width/image.size[0] * image.size[1])]
+        image = image.resize(new_size, Image.Resampling.BICUBIC)
+        pixels = Pixels.from_image(image)
+
+    console.print(pixels, overflow="crop")
+
+
+if __name__ == "__main__":
+    print_logo()
+    print_logo(True)
