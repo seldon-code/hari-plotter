1a9c307c42394627640c565df4148883fc01ee10
ivan <imtambovtcev@gmail.com>
2024-02-20 03:32:20 +0000

    + test plotter

--- tests/test_plotter.py
+++ tests/test_plotter.py
@@ -1,42 +1,57 @@
-# import networkx as nx
-# import numpy as np
-# import pytest
+import networkx as nx
+import numpy as np
+import pytest
 
-# from hari_plotter import HariGraph, Interface, Plotter
+from hari_plotter import HariGraph, Interface, Plotter, Simulation
+from hari_plotter.lazy_hari_graph import LazyHariGraph
 
 
-# class TestPlotter:
+class TestPlotter:
+    def test_main_plot(self):
+        S = Simulation.from_dir("tests/big_test")
+        plotter = Plotter.create_plotter(S)
+        plotter.interface.regroup(num_intervals=3, interval_size=1)
+        cl = {
+            "clustering_method": "Interval Clustering",
+            "clustering_parameters": ("Opinion", ),
+            "scale": ("Linear", ),
+            "parameter_boundaries": ((0.0, ), ),
+        }
+        plotter.add_plot(
+            "Scatter",
+            {
+                "parameters": ["Opinion", "Neighbor mean opinion"],
+                # "marker": "x",
+                "scale": ["Tanh", "Tanh"],
+                # "x_lim": [0, 5],
+                # "y_lim": [0, 2],
+            },
+            row=0,
+            col=0,
+        )
 
-#     @classmethod
-#     def setup_class(cls):
-#         H = HariGraph.read_network(
-#             'tests/network.txt', 'tests/opinions_0.txt')
-#         cls.H_plotter = Plotter.create_plotter(H)
+        plotter.add_plot(
+            "Static: Time line",
+            {
+                "parameters": ["Time", "Opinion"],
+            },
+            row=0,
+            col=1,
+        )
 
-#     def test_from_H(self):
-#         assert isinstance(self.H_plotter, Plotter)
-#         assert isinstance(self.H_plotter.interface, Interface)
+        # plotter.add_plot(
+        #     "Static: Opinions",
+        #     {
+        #         "scale": ["Linear", "Tanh"],
+        #         "clustering_settings": cl,
+        #     },
+        #     row=0,
+        #     col=1,
+        # )
+        plotter.size_ratios = [[3, 3], [3]]
 
-#     def test_draw(self):
-#         self.H_plotter.draw(mode=[])
-
-#     def test_1D_distribution(self):
-#         self.H_plotter.plot_1D_distribution(mode=[], x_parameter='opinion')
-#         self.H_plotter.plot_1D_distribution(
-#             mode=[], x_parameter='opinion', scale='tanh')
-
-#     def test_plot_2D_distribution(self):
-#         self.H_plotter.plot_2D_distribution(
-#             mode=[], x_parameter='opinion', y_parameter='neighbor_mean_opinion')
-#         self.H_plotter.plot_2D_distribution(
-#             mode=[], x_parameter='opinion', y_parameter='neighbor_mean_opinion', scale=['linear', 'tanh'])
-#         self.H_plotter.plot_2D_distribution(
-#             mode=[], x_parameter='opinion', y_parameter='neighbor_mean_opinion', scale=['tanh', 'tanh'])
-
-#     def test_plot_2D_distribution_extended(self):
-#         self.H_plotter.plot_2D_distribution_extended(
-#             mode=[], x_parameter='opinion', y_parameter='neighbor_mean_opinion')
-#         self.H_plotter.plot_2D_distribution_extended(
-#             mode=[], x_parameter='opinion', y_parameter='neighbor_mean_opinion', scale=['linear', 'tanh'])
-#         self.H_plotter.plot_2D_distribution_extended(
-#             mode=[], x_parameter='opinion', y_parameter='neighbor_mean_opinion', scale=['tanh', 'tanh'])
+        plotter.plot_dynamics(
+            mode=[],
+            save_dir="test_pics",
+            gif_path="test_pics/gif.gif",
+        )
