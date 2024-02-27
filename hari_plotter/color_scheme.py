07de5fb3c7340a8a068c837513b44a218d0865d3
ivan <imtambovtcev@gmail.com>
2024-02-27 22:47:27 +0000

    + cluster color

--- hari_plotter/color_scheme.py
+++ hari_plotter/color_scheme.py
@@ -85,14 +85,26 @@ class ColorScheme:
         self.default_scatter_marker = self.default_markers[0]
         self.default_none_marker = 'x'
 
-        self._scatter_color_cache = {}
-        self._scatter_cluster_marker_cache = {}
-        self._scatter_node_marker_cache = {}
+        self._cluster_color_cache = {}
+        self._node_color_cache = {}
+        self._cluster_marker_cache = {}
+        self._node_marker_cache = {}
 
     def clear(self):
-        self._scatter_color_cache = {}
-        self._scatter_cluster_marker_cache = {}
-        self._scatter_node_marker_cache = {}
+        self._cluster_color_cache = {}
+        self._node_color_cache = {}
+        self._cluster_marker_cache = {}
+        self._node_marker_cache = {}
+
+    def get_image(self, settings, group_number: int):
+        image = 'Current' if settings is None else settings.get(
+            'Image', group_number)
+        if image == 'Current':
+            return group_number
+        image = int(image)
+        if image < 0:
+            image += len(self.interface.groups)
+        return image
 
     @staticmethod
     def request_to_tuple(request):
@@ -112,7 +124,7 @@ class ColorScheme:
         return list(self._methods.keys())
 
     @method_logger('Distribution Color', modes=('Constant Color',))
-    def distribution_color(self, nodes, group_number: int, mode: str = None, settings=None):
+    def distribution_color(self, nodes, group_number: int, mode: str = None, settings: Union[dict, None] = None):
         mode = mode or 'Constant Color'
         if mode == 'Constant Color':
             if settings is not None and 'Color' in settings:
@@ -120,90 +132,115 @@ class ColorScheme:
             return self.default_distribution_color
 
     @method_logger('Scatter Marker', modes=('Constant Marker', 'Cluster Marker'))
-    def scatter_markers_nodes(self, nodes, group_number: int,  mode: str = None, settings=None):
+    def scatter_markers_nodes(self, nodes, group_number: int,  mode: str = None, settings: Union[dict, None] = None) -> Union[str, List[str]]:
         mode = mode or 'Constant Marker'
+        image = self.get_image(settings, group_number)
         if mode == 'Constant Marker':
             if settings is not None and 'Marker' in settings:
                 return settings['Marker']
             return self.default_scatter_marker
         elif mode == 'Cluster Marker':
-            if "clustering_settings" not in settings:
+            if settings is None or "clustering_settings" not in settings:
                 raise ValueError(
-                    'Settings are not formatted correctly. "clustering_settings" key is expected')
+                    f'Settings ({settings}) are not formatted correctly. "clustering_settings" key is expected')
+            # marker for nodes that are not in the cluster
+            none_marker = settings.get('None Marker', self.default_none_marker)
             cluster_request: Dict[str, Any] = {
-                'clustering_settings': settings['clustering_settings']}
+                'clustering_settings': settings['clustering_settings'], 'none_marker': none_marker}
             cluster_request_tuple = ColorScheme.request_to_tuple(
                 cluster_request)
-            if cluster_request_tuple not in self._scatter_cluster_marker_cache:
+            if cluster_request_tuple not in self._cluster_marker_cache:
                 cluster_names: list[str] = self.interface.cluster_tracker.get_unique_clusters(
                     settings['clustering_settings'])[0]
                 l = len(self.default_markers)
                 cluster_markers = {
                     cluster_name: self.default_markers[i % l] for i, cluster_name in enumerate(cluster_names)}
-                cluster_markers[None] = self.default_none_marker
-                self._scatter_cluster_marker_cache[cluster_request_tuple] = cluster_markers
-            image = settings.get('Image', group_number)
-            if image == 'Current':
-                image = group_number
-            image = int(image)
-            if image < 0:
-                image += len(self.interface.groups)
+                cluster_markers[None] = none_marker
+                self._cluster_marker_cache[cluster_request_tuple] = cluster_markers
 
-            request: Dict[str, Any] = {'group_number': group_number,
-                                       'clustering_settings': settings['clustering_settings']}
+            request: Dict[str, Any] = {'image': image,
+                                       'clustering_settings': settings['clustering_settings'], 'none_marker': none_marker}
             request_tuple = ColorScheme.request_to_tuple(request)
-            if request_tuple not in self._scatter_node_marker_cache:
+            if request_tuple not in self._node_marker_cache:
                 clustering: Clustering = self.interface.groups[image].clustering(
                     **settings['clustering_settings'])
-                # labels = clustering.cluster_labels
-                # mapping = clustering.get_cluster_mapping()
-                # print(f'{labels = }\n{mapping = }')
-                # print(f'{clustering.default_mapping_dict() = }')
                 cluster_mapping = clustering.mapping_dict()
-                cluster_to_marker_mapping = self._scatter_cluster_marker_cache[
+                cluster_to_marker_mapping = self._cluster_marker_cache[
                     cluster_request_tuple]
                 marker_mapping = {
                     node: cluster_to_marker_mapping[cluster] for node, cluster in cluster_mapping.items()}
-                self._scatter_node_marker_cache[request_tuple] = defaultdict(
-                    lambda: self.default_none_marker, marker_mapping)
+                self._node_marker_cache[request_tuple] = defaultdict(
+                    lambda: none_marker, marker_mapping)
 
-            return [self._scatter_node_marker_cache[request_tuple][node] for node in nodes]
+            return [self._node_marker_cache[request_tuple][node] for node in nodes]
 
-    @method_logger('Scatter Color', modes=('Constant Color', 'State Colormap',))
-    def scatter_colors_nodes(self, nodes, group_number: int,  mode: str = None, settings=None):
+    @method_logger('Scatter Color', modes=('Constant Color', 'State Colormap', 'Cluster Color'))
+    def scatter_colors_nodes(self, nodes, group_number: int,  mode: str = None, settings: Union[dict, None] = None) -> Union[str, list, List[list]]:
         mode = mode or 'Constant Color'
+        image = self.get_image(settings, group_number)
         if mode == 'Constant Color':
             if settings is not None and 'Color' in settings:
                 return settings['Color']
             return self.default_scatter_color
         elif mode == 'State Colormap':
-            if 'parameter' not in settings:
+            if settings is None or 'parameter' not in settings:
                 raise ValueError(
-                    'Settings are not formatted correctly. "parameter" key is expected')
-            image = settings.get('Image', group_number)
-            if image == 'Current':
-                image = group_number
-            image = int(image)
-            if image < 0:
-                image += len(self.interface.groups)
+                    f'Settings ({settings}) are not formatted correctly. "parameter" key is expected')
             request_settings = {'parameters': (settings['parameter'],)}
             request_settings['scale'] = settings.get('scale', 'Linear')
             colormap = settings.get('colormap', self.default_color_map)
+            none_color = settings.get('None Marker', self.default_none_color)
             request = {**request_settings,
-                       'colormap': colormap, 'image': image}
+                       'colormap': colormap, 'image': image, 'none_color': none_color}
             request_tuple = ColorScheme.request_to_tuple(request)
-            if request_tuple not in self._scatter_color_cache:
+            if request_tuple not in self._node_color_cache:
                 self.interface
                 data = self.interface.dynamic_data_cache[image][{
                     'method': 'calculate_node_values', 'group_number': group_number, 'settings': request_settings}]
-                # print(f'{data = }')
                 floats = data[settings['parameter']]
                 norm = colors.Normalize(vmin=min(floats), vmax=max(floats))
-                colormap = cm.get_cmap(colormap)
-                self._scatter_color_cache[request_tuple] = defaultdict(lambda: self.default_none_color, {node: colormap(
+                cm = plt.get_cmap(colormap)
+                self._node_color_cache[request_tuple] = defaultdict(lambda: none_color, {node: cm(
                     norm(value)) for node, value in zip(data['Nodes'], floats)})
-                # print(f'{self._scatter_color_cache[request_tuple] = }')
-            return [self._scatter_color_cache[request_tuple][node] for node in nodes]
+            return [self._node_color_cache[request_tuple][node] for node in nodes]
+        elif mode == 'Cluster Color':
+            if settings is None or "clustering_settings" not in settings:
+                raise ValueError(
+                    f'Settings ({settings}) are not formatted correctly. "clustering_settings" key is expected')
+            colormap = settings.get('colormap', self.default_color_map)
+            # color for nodes that are not in the cluster
+            none_color = settings.get('None Color', self.default_none_color)
+            cluster_request: Dict[str, Any] = {
+                'clustering_settings': settings['clustering_settings'], 'colormap': colormap, 'none_color': none_color}
+            cluster_request_tuple = ColorScheme.request_to_tuple(
+                cluster_request)
+            if cluster_request_tuple not in self._cluster_color_cache:
+                cluster_names: list[str] = self.interface.cluster_tracker.get_unique_clusters(
+                    settings['clustering_settings'])[0]
+                cm = plt.get_cmap(colormap)
+                color_indices = np.linspace(0, 1, len(cluster_names))
+
+                # Map each number to a color in the colormap
+                cluster_colors = {cluster_name: cm(
+                    color_index) for cluster_name, color_index in zip(cluster_names, color_indices)}
+                cluster_colors[None] = none_color
+                self._cluster_color_cache[cluster_request_tuple] = cluster_colors
+
+            request: Dict[str, Any] = {'image': image,
+                                       'clustering_settings': settings['clustering_settings'], 'colormap': colormap, 'none_color': none_color}
+            request_tuple = ColorScheme.request_to_tuple(request)
+            if request_tuple not in self._node_color_cache:
+                clustering: Clustering = self.interface.groups[image].clustering(
+                    **settings['clustering_settings'])
+                cluster_mapping = clustering.mapping_dict()
+                cluster_to_color_mapping = self._cluster_color_cache[
+                    cluster_request_tuple]
+                color_mapping = {
+                    node: cluster_to_color_mapping[cluster] for node, cluster in cluster_mapping.items()}
+                self._node_color_cache[request_tuple] = defaultdict(
+                    lambda: none_color, color_mapping)
+
+            return [self._node_color_cache[request_tuple][node] for node in nodes]
 
     @method_logger('Color Map', modes=('Independent Colormap',))
     def colorbar(self, nodes, group_number: int,  mode: str = None, settings=None):
